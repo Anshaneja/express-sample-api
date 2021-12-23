4def297be5637683a81d8b156b22e574eaf7cd32
Anshaneja <anshaneja05@gmail.com>
2021-12-23 20:31:09 +0530

    ERROR FIXED

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use("api/product" ,product);
 
-app.listen(PORT, ()=> console.log(`server is running at ${PORT}`));
+app.use("/api/product", product);
+
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
